3fe43dab27fc3b4681a2e4608d5ad9ee6df31559
Rvjonh <jonhvelasco3@gmail.com>
2023-01-28 10:49:48 -0400

    first version of covers for books

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,6 +138,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Forms from Crispy/Bootstrap
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
