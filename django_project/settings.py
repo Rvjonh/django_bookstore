8a9de05c259a8f0856f94d2987544fa45aef1929
Rvjonh <jonhvelasco3@gmail.com>
2023-01-29 18:06:11 -0400

    improve in user management 1

--- django_project/settings.py
+++ django_project/settings.py
@@ -164,8 +164,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-ACCOUNT_SESSION_REMEMBER = True
-ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+""" ACCOUNT_SESSION_REMEMBER = True """
+""" ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False """
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
