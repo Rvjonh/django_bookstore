d5ec980a39eea1fa0268eefe262c822da5285fad
Rvjonh <jonhvelasco3@gmail.com>
2023-01-29 10:00:48 -0400

    new homepage updated

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+from books.models import Book
+
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        last_5_books = Book.objects.filter().order_by("-id")[:5][::-1]
+        context["last_5_books"] = last_5_books
+        return context
 
 
 class AboutPageView(TemplateView):
