73849aac5183664a2da99bbf0006cb0ff1a163bd
Rvjonh <jonhvelasco3@gmail.com>
2023-02-03 12:14:25 -0400

    script helper for debug off attached

---
+++ login_confi_on_debug_off.py
@@ -0,0 +1,52 @@
+# Script to logging problems in DEBUG=OFF
+""" This dict must be copy and pasted in django_project.settings (the core project) to test posible problems in production mode. """
+""" You must not leave it in production, it should expose secret information about the server """
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    # 'disable_existing_loggers': False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            #'filters': ['require_debug_true'],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            #'filters': ['require_debug_false'],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "INFO",
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
